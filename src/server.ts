c54e95b2db101e1cc120d45a8c8543c6cb8d0be2
Elvis <elvis.e.amancio@gmail.com>
2021-03-24 16:58:10 -0300

    Debugando a aplicação e Criando categoria

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import express from "express";
 
-import createCourse from "./routes";
+import { categoriesRoutes } from "./routes/categories.routes";
 
 const app = express();
+app.use(express.json());
 
-app.get("/", createCourse);
+app.use(categoriesRoutes);
 
 app.listen(3333, () => {
   console.log("Server is Running!");
