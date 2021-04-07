f2a79ee0346f7642eec1334f01ffecb57612f465
Elvis <elvis.e.amancio@gmail.com>
2021-04-07 20:22:31 -0300

    Refatorando a listagem de categoria

---
+++ src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
@@ -0,0 +1,13 @@
+import { Category } from "../../Model/Category";
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
+
+// Todo UseCase deverá ter apenas um Controller
+class ListCategoriesUseCase {
+  constructor(private categoriesRepository: ICategoriesRepository) {}
+  execute(): Category[] {
+    const categories = this.categoriesRepository.list();
+    return categories;
+  }
+}
+
+export { ListCategoriesUseCase };
