2e825905b7c4b6978413987c50342975ceb03f27
Elvis <elvis.e.amancio@gmail.com>
2021-04-07 20:35:29 -0300

    Separando os repositórios

--- src/modules/cars/repositories/implementations/CategoriesRepository.ts
+++ src/modules/cars/repositories/implementations/CategoriesRepository.ts
@@ -1,8 +1,8 @@
-import { Category } from "../Model/Category";
+import { Category } from "../../Model/Category";
 import {
   ICategoriesRepository,
   ICreateCategoryDTO,
-} from "./ICategoriesRepository";
+} from "../ICategoriesRepository";
 
 class CategoriesRepository implements ICategoriesRepository {
   private categories: Category[] = [];
