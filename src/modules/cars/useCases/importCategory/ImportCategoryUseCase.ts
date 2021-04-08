e84371ae1afe6ad25a7467dac519f51524ddd0a1
Elvis <elvis.e.amancio@gmail.com>
2021-04-08 15:18:50 -0300

    Lendo os dados do upload

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,20 +1,50 @@
 import csvParse from "csv-parse";
 import fs from "fs";
 
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
+
+interface IImportCategory {
+  name: string;
+  description: string;
+}
+
 // Todo UseCase deverá ter apenas um Controller
 class ImportCategoryUseCase {
-  execute(file: Express.Multer.File): void {
-    // Cria Stream do arquivo
-    const stream = fs.createReadStream(file.path);
+  constructor(private categoriesRepository: ICategoriesRepository) {}
 
-    // Responsavel por ler as linhas do arquivo
-    const parseFile = csvParse();
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      // Cria Stream do arquivo
+      const stream = fs.createReadStream(file.path);
+      const categories: IImportCategory[] = [];
 
-    stream.pipe(parseFile);
+      // Responsavel por ler as linhas do arquivo
+      const parseFile = csvParse();
 
-    parseFile.on("data", async (line) => {
-      console.log("line=>", line);
+      // Pipe fica responsável por pegar o que foi lido no Stream e enviar pra
+      // outro lugar. Que no caso foi entregue para o parseFile fazer a leitura.
+      stream.pipe(parseFile);
+
+      parseFile
+        .on("data", async (line) => {
+          const [name, description] = line;
+          categories.push({
+            name,
+            description,
+          });
+        })
+        .on("end", () => {
+          resolve(categories);
+        })
+        .on("error", (error) => {
+          reject(error);
+        });
     });
+  }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file);
+    console.log("categories => =>", categories);
   }
 }
 
